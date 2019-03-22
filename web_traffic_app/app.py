84c709d63b8e6ff401af9cf048c931bd63141a1f
CrDesigns93 <38084221+CrDesigns93@users.noreply.github.com>
2019-03-22 01:17:03 -0700

    Visual Edit

--- web_traffic_app/app.py
+++ web_traffic_app/app.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/model", methods=["POST", "GET"] )
+def model():
+    return render_template('model.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 if (__name__) == '__main__':
     app.run(debug=False)
