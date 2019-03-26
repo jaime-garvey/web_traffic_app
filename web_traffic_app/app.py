025e2fdede6da1bb18906e4fc550ad547ff9c8c2
Jaime <jaimemgarvey@gmail.com>
2019-03-26 02:19:51 -0700

    add predict

--- web_traffic_app/app.py
+++ web_traffic_app/app.py
@@ -13,6 +13,18 @@ def index():
 def model():
     return render_template('model.html')
 
+@app.route("/recommend-youtube", methods=["GET"] )
+def youtube():
+    return render_template('youtube.html')
+
+# @app.route("/recommend-apparel", methods=["POST", "GET"] )
+# def model():
+#     return render_template('apparel.html')
+#
+# @app.route("/recommend-wishlist", methods=["POST", "GET"] )
+# def model():
+#     return render_template('wishlist.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
